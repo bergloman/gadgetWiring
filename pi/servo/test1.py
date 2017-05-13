784b15511520eabc0cea5cd284664a1dbb0c3e32
bergloman <bergloman@gmail.com>
2017-05-13 22:04:49 +0200

    Tests

--- pi/servo/test1.py
+++ pi/servo/test1.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-pin_val=11
+pin_val = 11
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin_val, GPIO.OUT)
@@ -11,7 +11,7 @@ try:
         GPIO.output(pin_val, 1)
         time.sleep(0.0015)
         GPIO.output(pin_val, 0)
-        time.sleep(2)
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
