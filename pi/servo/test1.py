b0aedd3b6dcd4dd45c4272f9c4b2578933469428
bergloman <bergloman@gmail.com>
2017-05-13 22:10:41 +0200

    added comments

--- pi/servo/test1.py
+++ pi/servo/test1.py
@@ -1,3 +1,7 @@
+# pin 11 is 6th in the left column (to orange wire of the motor)
+# gnd is 5th in the left column (to brown wire of the motor)
+# 5V is 1st in the right column (to red wire of the motor)
+
 import RPi.GPIO as GPIO
 import time
 
