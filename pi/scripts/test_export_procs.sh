55f9d0dd6c6bccf17af4bdaa1e834de14bbc2253
Viktor Jovanoski <bergloman@users.noreply.github.com>
2017-09-08 06:52:39 +0200

    Update test_export_procs.sh

--- pi/scripts/test_export_procs.sh
+++ pi/scripts/test_export_procs.sh
@@ -1,1 +1,6 @@
-tail -n +8 out.txt | awk '{printf "%s,%s,%.2f,%.2f\n",$(12),$(2), $(9), $(10)}'
+#tail -n +8 out.txt | awk '{printf "%s,%s,%.2f,%.2f\n",$(12),$(2), $(9), $(10)}'
+
+
+NOW=$(date -Is)
+tail -n +8 out.txt | awk '{printf ",%s,%s,%.2f,%.2f\n",$(12),$(2), $(9), $(10)}' | awk -v now=$NOW '{print now$0}'
+
