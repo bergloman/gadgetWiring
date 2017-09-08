a3dfe984381ce1f197c28ca4f9a428645b9d9806
bergloman <bergloman@gmail.com>
2017-09-08 16:53:58 +0200

    more work

--- pi/scripts/test_export_procs.sh
+++ pi/scripts/test_export_procs.sh
@@ -2,5 +2,5 @@
 
 
 NOW=$(date -Is)
-tail -n +8 out.txt | awk '{printf ",%s,%s,%.2f,%.2f\n",$(12),$(2), $(9), $(10)}' | awk -v now=$NOW '{print now$0}'
+top -bn1 | awk '{printf ",%s,%s,%.2f,%.2f\n",$(12),$(2), $(9), $(10)}' | awk -v now=$NOW '{print now$0}'
 
