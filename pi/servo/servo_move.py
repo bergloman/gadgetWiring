a2b71560a2a87af4c25d1bc2f1a700b888470f3f
bergloman <bergloman@users.noreply.github.com>
2018-12-06 12:41:10 +0100

    Create servo_move.py

---
+++ pi/servo/servo_move.py
@@ -0,0 +1,33 @@
+# https://tutorials-raspberrypi.com/raspberry-pi-servo-motor-control/
+#
+# The period is 20ms long.
+# In addition, a duty cycle of between 5% and 10% of the period is expected.
+# Thus the pulse duration is between 1ms and 2ms. Usually, servos can rotate
+# between 0 and 180. So we have to adjust the pulse length in between.
+# A length of 1.5ms (7.5%) thus gives an angle of 90. And all at precisely 50x per second (50Hz).
+
+
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+
+SLEEP_TIME=1.2
+
+angle=90
+
+dc = 2.5 + 10 * (180 - angle) / 180
+print(dc)
+try:
+  p.ChangeDutyCycle(dc)
+  time.sleep(SLEEP_TIME)
+  p.stop()
+  GPIO.cleanup()
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
